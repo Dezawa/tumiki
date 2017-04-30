5f12793c99f34270d31d948ccf2b54d5c4688938
Dezawa Shin <dezawa@aliadne.net>
2017-04-30 17:35:15 +0900

    Rspec で columnのテストができるようになった controller とか view とかの環境のテストだから、

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,28 @@
+# coding: utf-8
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+require 'i18n'
+require 'action_view'
+require 'action_controller'
+require 'active_record'
+#require 'action_view/helpers/form_helpe'
 require 'tumiki'
+require 'pp'
+
+class Block < ActiveRecord::Base
+end
+class BlockController <  ActionController::Base
+  include Tumiki::ApplicationControllerExtension
+  include Tumiki
+  attr_accessor :params
+  before_filter :tumiki_instanse_variable
+  def initialize(*args)
+    super *args
+    @params = {}
+  end
+
+  def index_columns
+    column :name, label: "名前"
+    column :address
+  end
+end
+  
